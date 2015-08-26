4911c93071570a8b83cdf2665a5c5c3e3ee8f582
GS Shahid <gs@teknuk.com>
2015-08-26 14:56:23 +0500

    added devise_invitable gem

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'bootstrap-sass', '~> 3.3.5'
 gem 'bootstrap-generators', '~> 3.3.4'
 gem 'devise'
 gem 'devise-bootstrap-views'
+gem 'devise_invitable'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   has_many :projects, foreign_key: :owner_id
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
