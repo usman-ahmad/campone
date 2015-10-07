4b84b6b9f773b150d1dc06a44a0c67f3cae02f43
GS Shahid <gs@teknuk.com>
2015-10-07 20:13:07 +0500

    invitations - generated devise_invitable views and added name field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:accept_invitation) << :name
     devise_parameter_sanitizer.for(:account_update) << :avatar
   end
 end
