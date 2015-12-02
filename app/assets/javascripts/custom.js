7e693f3061778103332a181680649070e6b939a9
Jazib Bashir <jb@teknuk.com>
2015-12-02 12:33:43 +0500

    designs - added html for new project and fixed some css issues

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -6,9 +6,19 @@ var ready = function () {
     $(".anchor-container a").click(function (e) {
         e.stopPropagation();
     });
-    $(function() {
-        $( "#sortable" ).sortable();
+    $(function () {
+        $("#sortable").sortable();
     });
+    function showGroupFieldsfiles() {
+        var hideField_discussion = $('#hideField-files');
+        var showField_discussion = $('#showField-files');
+        $('.newgroup_icon').click(function () {
+            showField_discussion.hide();
+            hideField_discussion.removeClass('hide');
+        });
+    }
+
+    showGroupFieldsfiles();
 };
 $(document).ready(ready);
 $(document).on('page:load', ready);
