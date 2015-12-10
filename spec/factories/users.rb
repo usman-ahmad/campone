0b029907665f1a1a1c473390842f809dfe0a22e7
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-10 18:00:05 +0500

    add user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
+  names = ['waqas','salman','sunny','asad','irtaza']
   factory :user do
-    name "Ateek"
+    sequence(:name) { |n| names[(n % names.size)]}
     sequence(:email) { |n| "teknuk#{n}@ruby.com"}
     password "secretpassword"
     password_confirmation "secretpassword"
