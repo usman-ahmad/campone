278d65df42a737ddf0172b257832a7b86a101713
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-10 19:56:10 +0500

    add project factory and its relation with user factory

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,8 +1,10 @@
 FactoryGirl.define do
+  names = ['desktop medical store MIS','online medical store MIS','mobile app of MIS','face recognition']
+  description = ['it should be created on .net','it should be created on ROR','it should be created on android','this can be create on any language']
   factory :project do
-    name "MyString"
-description "MyText"
-owner_id 1
-  end
+    sequence(:name) { |n| names[(n % names.size)]}
+    sequence(:description) { |n| description[(n % description.size)]}
 
+    association :owner, factory: :user
+  end
 end
