ec43c7bf32d9306278d1b8f2803dcbc70aad3cda
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-11 11:38:39 +0500

    add association between task and project factory

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,10 +1,26 @@
 FactoryGirl.define do
   names = ['desktop medical store MIS','online medical store MIS','mobile app of MIS','face recognition']
   description = ['it should be created on .net','it should be created on ROR','it should be created on android','this can be create on any language']
+
   factory :project do
     sequence(:name) { |n| names[(n % names.size)]}
     sequence(:description) { |n| description[(n % description.size)]}
 
     association :owner, factory: :user
   end
+
+  factory :project_with_single_task, parent: :project do
+    after(:create) do |project|
+    create(:low_priority_task, project: project )
+    end
+  end
+
+  factory :project_with__many_tasks, parent: :project do
+    after(:create) do |project|
+      create(:low_priority_task,project: project )
+      create(:medium_priority_task,project: project )
+      create(:high_priority_task,project: project )
+    end
+  end
+
 end
