766edc1570a3767c59c5c819c25f70c1e80f8312
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-11 18:22:17 +0500

    add discussion factory

--- spec/factories/discussions.rb
+++ spec/factories/discussions.rb
@@ -1,9 +1,21 @@
 FactoryGirl.define do
+  titles = ['intelligent features','mokups','client requirments','task deadlines']
   factory :discussion do
-    title "MyString"
-content "MyText"
-private false
-project nil
+    sequence(:title) { |n| titles[(n % titles.size)]}
+    content "client should be setisfy and project should be created on timeline"
+    association :discussion_group, factory: :discussion_group
+    project
+
+  trait :private do
+    private true
   end
 
+  trait :none_private do
+    private false
+  end
+
+  factory :private_discussion, traits: [:private]
+  factory :none_private_discussion, traits: [:none_private]
+
+  end
 end
