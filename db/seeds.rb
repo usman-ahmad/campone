5ea9467c30cac95f2a77b57077fa02d690598473
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-12 19:42:02 +0500

    create seeds through factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# User email = teknuk-salman@ruby.com
+# password = secretpassword
+require 'factory_girl'
+
+puts 'db/seeds started'
+
+
+puts 'creating user'
+user = FactoryGirl.create_list(:user,5)
+
+puts 'creating Project'
+project = FactoryGirl.create(:project, owner: user.first)
+
+puts 'creating project_with_single_task'
+single_task_project = FactoryGirl.create(:project_with_single_task, owner: user.first)
+
+puts 'creating project_with_many_tasks'
+many_tasks_project = FactoryGirl.create(:project_with_many_tasks, owner: user.first)
+
+puts 'creating project_with_discussions'
+project_with_discussions = FactoryGirl.create(:project_with_discussions, owner: user.first)
+
+puts 'creating project_with_task_discussions'
+project_with_task_discussions = FactoryGirl.create(:project_with_task_discussions, owner: user.first)
+
+puts 'creating discussion'
+discussion = FactoryGirl.create(:private_discussion, project: project, commenter: project.owner)
+
+projects =[project,single_task_project, many_tasks_project, project_with_discussions, project_with_task_discussions]
+
+puts 'creating user_discussion and invitations'
+user.each do |user|
+  FactoryGirl.create(:user_discussion, user: user, discussion: discussion)
+end
+
+FactoryGirl.create(:organizer_invitation, user: user[1], project: projects[0])
+FactoryGirl.create(:team_player_invitation, user: user[1], project: projects[1])
+FactoryGirl.create(:contributor_invitation, user: user[2], project: projects[2])
+FactoryGirl.create(:organizer_invitation, user: user[4], project: projects[3])
+FactoryGirl.create(:contributor_invitation, user: user[4], project: projects[4])
