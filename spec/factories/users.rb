f578a1a7051f76f0f86373cb0808ea56da45880b
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-12 13:54:32 +0500

    refactor user factory to generate proper user email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   names = ['waqas','salman','sunny','asad','irtaza']
   factory :user do
     sequence(:name) { |n| names[(n % names.size)]}
-    sequence(:email) { |n| "teknuk#{n}@ruby.com"}
+    sequence(:email) { |n| "teknuk-#{names[(n % names.size)]}@ruby.com"}
     password "secretpassword"
     password_confirmation "secretpassword"
   end
