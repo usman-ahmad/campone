b08c9129300420a7f8c1989eba2af762000a1a57
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-12 14:36:40 +0500

    refactor comments factory to associate with user(commenter)

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   comments = ['very nice','i dont link','it can be improve']
   factory :comment do
     sequence(:content) { |n| comments[(n % comments.size)]}
+    user
   end
 end
