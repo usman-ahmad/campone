0187bbfc41d56b35dd2e5ac3b17f948107aac04d
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-12 17:51:48 +0500

    add inivitation factory to invite users on projects

--- spec/factories/invitations.rb
+++ spec/factories/invitations.rb
@@ -1,8 +1,23 @@
 FactoryGirl.define do
   factory :invitation do
-    project nil
-user nil
-role 1
+    project
+    user
+
+    trait :organizer do
+      role :organizer
+      end
+
+    trait :team_player do
+      role :team_player
+    end
+
+    trait :contributor do
+      role :contributor
+    end
+
+    factory :organizer_invitation,   traits: [:organizer]
+    factory :team_player_invitation, traits: [:team_player]
+    factory :contributor_invitation, traits: [:contributor]
   end
 
 end
