5699814a1fca158adcc5140a7849478c8fe9834e
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-18 14:33:02 +0500

    added validation in Discussion Model

--- app/models/user_discussion.rb
+++ app/models/user_discussion.rb
@@ -1,4 +1,6 @@
 class UserDiscussion < ActiveRecord::Base
   belongs_to :user
   belongs_to :discussion
+
+  validates :user, uniqueness: { scope: :discussion, message: "Already invited on this Discussion." }
 end
