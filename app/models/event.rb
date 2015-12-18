0bddfccdda10a823da0ab60332e640bad3f08678
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-18 12:43:58 +0500

    added Event Model specs and some validations in Event Model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,10 @@ class Event < ActiveRecord::Base
   belongs_to :project
 
   validates :title, :due_at, presence: true
+  validate :due_date
+
+  def due_date
+    errors.add(:due_at, "can't be in the past") if
+        due_at < Date.today if due_at.present?
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){ create(:user) }
+  let(:project){ create(:project, owner: user) }
+
+  describe "due_at date" do
+    it 'should not allow due date in past' do
+      build(:event, project: project, due_at: (Date.today - 1) ).should_not be_valid
+    end
+
+    it 'should present due date' do
+      build(:event, project: project, due_at: nil ).should_not be_valid
+    end
+  end
+  
+  it 'should have title' do
+    build(:event, project: project, title:nil ).should_not be_valid
+  end
+
+  it 'should create event' do
+    event = create(:event, project: project, title:'Project testing')
+    event.title.should eq ('Project testing')
+    event.due_at.should_not be < Date.today
+  end
 end
