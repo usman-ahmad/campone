c6f35f10f263ab565fe2ec15ff86f5a3a30e1a34
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-18 13:03:26 +0500

    added Discussion Model specs

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Discussion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){ create_list(:user, 3) }
+  let(:project){ create(:project, owner: user.first) }
+
+  describe 'title' do
+    it 'should present' do
+      build(:none_private_discussion,project: project, commenter: project.owner, title:nil ).should_not be_valid
+    end
+  end
+  describe 'invite on discussion' do
+    let(:discussion) {create(:private_discussion,project: project, commenter: project.owner)}
+
+    it 'should share with users' do
+      user.each do |user|
+        FactoryGirl.create(:user_discussion, user: user, discussion: discussion)
+      end
+         discussion.users.count.should eq(3)
+    end
+
+  end
 end
