6a14fba06e3ceeb049f88fbf21d18778505bddf4
Muhammad Ateeq <ateeq@teknuk.com>
2015-12-22 11:55:48 +0500

    refactor login specs and resolve issue of login from rspecs

--- spec/features/login_feature_spec.rb
+++ spec/features/login_feature_spec.rb
@@ -4,4 +4,12 @@ describe 'login', type: :feature, :js => true do
   it 'should login to dashboard' do
     create_admin_user_and_login
   end
+
+  it 'should login to dashboard' do
+    visit   root_path
+    fill_in 'login_user_email',    with: user.email
+    fill_in 'login_user_password', with: "secretpassword"
+    click_button 'Submit'
+    expect(page.current_path).to eq projects_path
+  end
 end
