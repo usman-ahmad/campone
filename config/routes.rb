9f6dcf7559848f84929aaf83b33b0ecd8cedbc1f
Muhammad Ateeq <ateeq@teknuk.com>
2016-01-18 14:59:23 +0500

    resolve conflicts after renaming of webhooks controller

--- config/routes.rb
+++ config/routes.rb
@@ -44,8 +44,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'notifications' => 'notifications#index'
 
-  #resource to receive github messages as a post request.
-  resource :github_webhooks, only: :create, defaults: { formats: :json }
+  #resource to receive VCS messages as a post request.
+  post 'webhooks/:vcs_name' => 'webhooks#create', defaults: {formats: :json}
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/controllers/webhooks_controller_spec.rb
+++ spec/controllers/webhooks_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe GithubwebhooksController, type: :controller do
+RSpec.describe WebhooksController, type: :controller do
 
 end
