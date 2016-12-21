203cc9a82fe45d589975dabf6cb9852bba19456f
GS Shahid <gs@teknuk.com>
2016-12-21 21:26:05 +0500

    specs - delete test files from file system

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -27,6 +27,16 @@ RSpec.configure do |config|
   end
 
   config.after(:each) do
+    # Test files are not properly cleaned from file system. Paperclip adds some hooks
+    # A before_destroy hook `queue_all_for_delete` saves all files in instance variable `@queued_for_delete`
+    # Then another hook (after_commit, on: destroy) deletes the @queued_for_delete files from file system.
+    # These hooks properly works if we call `attachment.destroy` in rails console
+    # But having issue when rspec implicitly destroys objects (Garbage collection)
+    # Similar issue is reported (after_commit not being called) but this has been fixed in rails 5.0
+    # https://github.com/thoughtbot/paperclip/issues/1445
+
+    # Workaround for destroying test files, destroy all models having paperclip attachments
+    Attachment.destroy_all
     DatabaseCleaner.clean
   end
 end
