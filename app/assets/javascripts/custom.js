03dce193bf647c300c4e1ea17afb8ab793957323
Jazib Bashir <jb@teknuk.com>
2017-03-21 15:58:18 +0500

    public_pages - added map section

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -135,3 +135,20 @@ $(document).on('turbolinks:load', function () {
         }
     });
 });
+function initialize() {
+    var myLatlng = new google.maps.LatLng(31.482288, 74.395823);
+    var map = document.getElementById('map');
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(map, mapOptions);
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: "TEKNUK"
+    });
+    marker.setMap(map);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
