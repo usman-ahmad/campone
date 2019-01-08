89d9ae78c62f5dc65b846ca88dd0bf55ffc10b8b
Usman Ahmad <ua@teknuk.com>
2019-01-09 01:27:47 +0500

    mail_service - added sendgrid for smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,11 +65,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: ENV['HOST']}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'nimblein.com',
-      user_name:            ENV['GMAIL_USER_NAME'],
-      password:             ENV['GMAIL_PASSWORD'],
+      address:              ENV['SMTP_ADDRESS'],
+      port:                 ENV['SMTP_PORT'],
+      domain:               ENV['SMTP_DOMAIN'],
+      user_name:            ENV['SMTP_USER_NAME'],
+      password:             ENV['SMTP_PASSWORD'],
       authentication:       'plain',
       enable_starttls_auto: true
   }
